5df60bbddcd8728972f76de1f475cb37e1c45fc1
UniQueKakarot <iverullrik92@gmail.com>
2017-08-26 22:53:55 +0200

    Cleaning up abit

--- TPV_Source/logging_test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import logging
-
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
-logging.error('Test')
